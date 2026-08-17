890e4cdaddafa1c4f858eb2700b25b5b2bd9dc95
wancong <1018957763@qq.com>
2026-08-18 01:26:57 +0800

    chore: bump version to 0.1.22

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="half_sample",
-    version="0.1.21",
+    version="0.1.22",
     author="kunde",
     author_email="gzkunde@163.com",
     description="sample data and analysis",
